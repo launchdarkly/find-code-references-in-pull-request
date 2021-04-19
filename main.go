89221039c02b056b2c939f06e67423b81e0f5118
Dan O'Brien <dobrien@launchdarkly.com>
2021-04-19 15:04:27 -0400

    do not get summary

--- main.go
+++ main.go
@@ -85,7 +85,7 @@ func getFlags(config *lcr.Config) (ldapi.FeatureFlags, []string, error) {
 			envString = envString + "&"
 		}
 	}
-	url := config.LdInstance + "/api/v2/flags/" + config.LdProject + "?" + envString
+	url := config.LdInstance + "/api/v2/flags/" + config.LdProject + "?" + envString + "&summary=0"
 	fmt.Println(url)
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
