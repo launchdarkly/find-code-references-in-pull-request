3a1ab77894f32eaedfc731072f23b5256a542577
Dan O'Brien <dobrien@launchdarkly.com>
2021-09-18 17:44:43 -0400

    test print

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	ldapi "github.com/launchdarkly/api-client-go"
 )
@@ -29,6 +30,7 @@ func NewClient(token string, apiHost string, oauth bool) (*Client, error) {
 }
 
 func (w *Client) WrapContext(ctx context.Context) context.Context {
+	fmt.Println(w.ApiKey)
 	auth := map[string]ldapi.APIKey{
 		"ApiKey": {Key: w.ApiKey},
 	}
