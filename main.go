dd7b2e310097246525e542b2114937e7bbc8954e
Jaz White <jwhite@launchdarkly.com>
2023-06-28 10:05:33 -0400

    remove unused functions

--- main.go
+++ main.go
@@ -5,18 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
-	"time"
 
 	"github.com/google/go-github/github"
 	ldapi "github.com/launchdarkly/api-client-go"
-	lc "github.com/launchdarkly/cr-flags/client"
 	ghc "github.com/launchdarkly/cr-flags/comments"
-	"github.com/launchdarkly/cr-flags/config"
 	lcr "github.com/launchdarkly/cr-flags/config"
 	ldiff "github.com/launchdarkly/cr-flags/diff"
 	"github.com/launchdarkly/ld-find-code-refs/coderefs"
@@ -203,167 +198,5 @@ func failExit(err error) {
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
-	}
-}
-
-func ptr(v interface{}) *interface{} { return &v }
-
-func mergeKeys(a map[string][]string, b map[string][]string) {
-	for k, v := range b {
-		a[k] = v
-	}
-}
-
-func handleRateLimit(apiCall func() (interface{}, *http.Response, error)) (interface{}, *http.Response, error) {
-	obj, res, err := apiCall()
-	for retryCount := 0; res != nil && res.StatusCode == http.StatusTooManyRequests && retryCount < MAX_429_RETRIES; retryCount++ {
-		log.Println("[DEBUG] received a 429 Too Many Requests error. retrying")
-		resetStr := res.Header.Get("X-RateLimit-Reset")
-		resetInt, parseErr := strconv.ParseInt(resetStr, 10, 64)
-		if parseErr != nil {
-			log.Println("[DEBUG] could not parse X-RateLimit-Reset header. Sleeping for a random interval.")
-			randomRetrySleep()
-		} else {
-			resetTime := time.Unix(0, resetInt*int64(time.Millisecond))
-			sleepDuration := time.Until(resetTime)
-
-			// We have observed situations where LD-s retry header results in a negative sleep duration. In this case,
-			// multiply the duration by -1 and add a random 200-500ms
-			if sleepDuration <= 0 {
-				log.Printf("[DEBUG] received a negative rate limit retry duration of %s. Sleeping for an additional 200-500ms", sleepDuration)
-				sleepDuration = -1*sleepDuration + getRandomSleepDuration()
-			}
-			log.Println("[DEBUG] sleeping", sleepDuration)
-			time.Sleep(sleepDuration)
-		}
-		obj, res, err = apiCall()
-	}
-	return obj, res, err
-
-}
-
-var randomRetrySleepSeeded = false
-
-// Sleep for a random interval between 200ms and 500ms
-func getRandomSleepDuration() time.Duration {
-	if !randomRetrySleepSeeded {
-		rand.Seed(time.Now().UnixNano())
-	}
-	n := rand.Intn(300) + 200
-	return time.Duration(n) * time.Millisecond
-}
-
-func randomRetrySleep() {
-	time.Sleep(getRandomSleepDuration())
-}
-
-func split(r rune) bool {
-	return r == ',' || r == ' '
-}
-
-func processCustomProps(flags ldapi.FeatureFlags, existingComment *github.IssueComment, config *config.Config, flagsRef ghc.FlagsRef, event *github.PullRequestEvent) {
-	var existingFlagKeys []string
-	if existingComment != nil && strings.Contains(*existingComment.Body, "<!-- flags") {
-		lines := strings.Split(*existingComment.Body, "\n")
-		for _, line := range lines {
-			if strings.Contains(line, "<!-- flags:") {
-				flagLine := strings.SplitN(line, ":", 2)
-				existingFlagKeys = append(existingFlagKeys, strings.FieldsFunc(flagLine[1], split)...)
-				existingFlagKeys = existingFlagKeys[:len(existingFlagKeys)-1]
-			}
-		}
-		customProp := "ldcrc:" + strings.Join(config.Repo, "/")
-		var currentCustomProp ldapi.CustomProperty
-
-	FlagRefLoop:
-		for k := range flagsRef.FlagsAdded {
-			// Remove flags from existing flags. We only want non-existant ones to be left
-			for i, v := range existingFlagKeys {
-				if v == k {
-					existingFlagKeys = append(existingFlagKeys[:i], existingFlagKeys[i+1:]...)
-					break
-				}
-			}
-			for i := range flags.Items {
-				if flags.Items[i].Key == k {
-					existingProps := flags.Items[i].CustomProperties
-					currentCustomProp = existingProps[customProp]
-					for _, v := range existingProps[customProp].Value {
-						if v == strconv.Itoa(*event.PullRequest.Number) {
-							continue FlagRefLoop
-						}
-					}
-				}
-			}
-			patchingCustomProp := append(currentCustomProp.Value, strconv.Itoa(*event.PullRequest.Number))
-			customProperty := ldapi.CustomProperty{
-				Name:  customProp,
-				Value: patchingCustomProp,
-			}
-			customPatch := make(map[string]ldapi.CustomProperty)
-			customPatch[customProp] = customProperty
-			patch := ldapi.PatchOperation{
-				Op:    "replace",
-				Path:  fmt.Sprintf("/customProperties/%s", customProp),
-				Value: ptr(customPatch),
-			}
-			ldClient, err := lc.NewClient(config.ApiToken, config.LdInstance, false)
-			if err != nil {
-				log.Println(err)
-			}
-			patchComment := ldapi.PatchComment{
-				Patch:   []ldapi.PatchOperation{patch},
-				Comment: "PR Commentor",
-			}
-			_, _, err = handleRateLimit(func() (interface{}, *http.Response, error) {
-				return ldClient.Ld.FeatureFlagsApi.PatchFeatureFlag(ldClient.Ctx, config.LdProject, k, patchComment)
-			})
-			if err != nil {
-				log.Println(err)
-				os.Exit(1)
-			}
-		}
-		for _, orphanKey := range existingFlagKeys {
-			for i := range flags.Items {
-				if flags.Items[i].Key == orphanKey {
-					existingProps := flags.Items[i].CustomProperties
-					currentCustomProp = existingProps[customProp]
-					break
-				}
-			}
-			var newProps []string
-			for i, v := range currentCustomProp.Value {
-				if v == strconv.Itoa(*event.PullRequest.Number) {
-					newProps = append(currentCustomProp.Value[:i], currentCustomProp.Value[i+1:]...)
-					break
-				}
-			}
-			customProperty := ldapi.CustomProperty{
-				Name:  customProp,
-				Value: newProps,
-			}
-			customPatch := make(map[string]ldapi.CustomProperty)
-			customPatch[customProp] = customProperty
-			patch := ldapi.PatchOperation{
-				Op:    "replace",
-				Path:  "/customProperties",
-				Value: ptr(customPatch),
-			}
-			ldClient, err := lc.NewClient(config.ApiToken, config.LdInstance, false)
-			if err != nil {
-				log.Println(err)
-			}
-			patchComment := ldapi.PatchComment{
-				Patch:   []ldapi.PatchOperation{patch},
-				Comment: "PR Commentor",
-			}
-			_, _, err = handleRateLimit(func() (interface{}, *http.Response, error) {
-				return ldClient.Ld.FeatureFlagsApi.PatchFeatureFlag(ldClient.Ctx, config.LdProject, orphanKey, patchComment)
-			})
-			if err != nil {
-				log.Println(err)
-				os.Exit(1)
-			}
-		}
 	}
 }
