5bda4fd8a5306a4756f5ad19fd710419de90b688
Jaz <jwhite@launchdarkly.com>
2023-11-13 13:16:02 -0500

    rename some things

--- internal/extinctions/extinctions.go
+++ internal/extinctions/extinctions.go
@@ -1,21 +1,21 @@
 package extinctions
 
 import (
-	lflags "github.com/launchdarkly/find-code-references-in-pull-request/flags"
+	"github.com/launchdarkly/find-code-references-in-pull-request/flags"
 	"github.com/launchdarkly/find-code-references-in-pull-request/search"
 	"github.com/launchdarkly/ld-find-code-refs/v2/options"
-	lsearch "github.com/launchdarkly/ld-find-code-refs/v2/search"
+	ld_search "github.com/launchdarkly/ld-find-code-refs/v2/search"
 )
 
-func CheckExtinctions(opts options.Options, builder *lflags.ReferenceBuilder) error {
-	flags := make([]string, 0, len(builder.RemovedFlagKeys()))
+func CheckExtinctions(opts options.Options, builder *flags.ReferenceBuilder) error {
+	flagKeys := make([]string, 0, len(builder.RemovedFlagKeys()))
 
-	matcher, err := search.GetMatcher(opts, flags, nil)
+	matcher, err := search.GetMatcher(opts, flagKeys, nil)
 	if err != nil {
 		return err
 	}
 
-	references, err := lsearch.SearchForRefs(opts.Dir, matcher)
+	references, err := ld_search.SearchForRefs(opts.Dir, matcher)
 	if err != nil {
 		return err
 	}
