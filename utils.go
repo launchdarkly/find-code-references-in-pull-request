9c02e4601597f1ef5f082ab2d37765abf0bf8637
Daniel OBrien <dobrien@launchdarkly.com>
2023-11-22 08:35:16 -0500

    test deleting org field on event payload

--- utils.go
+++ utils.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"os"
 
 	"github.com/google/go-github/github"
 )
+
+func clearJSONRepoOrgField(body []byte) []byte {
+	// workaround for https://github.com/google/go-github/issues/131
+	var o map[string]interface{}
+	dec := json.NewDecoder(bytes.NewReader(body))
+	dec.UseNumber()
+	dec.Decode(&o)
+	if o != nil {
+		repo := o["repository"]
+		if repo != nil {
+			if repo, ok := repo.(map[string]interface{}); ok {
+				delete(repo, "organization")
+			}
+		}
+	}
+	b, _ := json.MarshalIndent(o, "", "  ")
+	return b
+}
 
 // pulled from ld-find-code-refs github action
 func parseEvent(path string) (*github.PullRequestEvent, error) {
@@ -21,7 +40,7 @@ func parseEvent(path string) (*github.PullRequestEvent, error) {
 		return nil, err
 	}
 	var evt github.PullRequestEvent
-	err = json.Unmarshal(eventJsonBytes, &evt)
+	err = json.Unmarshal(clearJSONRepoOrgField(eventJsonBytes), &evt)
 	if err != nil {
 		return nil, err
 	}
