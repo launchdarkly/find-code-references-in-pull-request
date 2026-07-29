0e8a26a8739db87a6a6ecaee259fc25284e09fcf
Jaz White <jaz@fieldguide.io>
2026-07-29 13:41:06 -0400

    allow skip posting comment

--- main.go
+++ main.go
@@ -86,18 +86,21 @@ func main() {
 	setOutputs(config, flagsRef)
 
 	// Add comment
-	gha.StartLogGroup("Processing comment...")
-	existingComment := checkExistingComments(event, config, ctx)
-	buildComment := ghc.ProcessFlags(flagsRef, flags, config)
-	postedComments := ghc.BuildFlagComment(buildComment, flagsRef, existingComment)
-	if postedComments != "" {
-		comment := github.IssueComment{
-			Body: &postedComments,
-		}
+	postedComments := ""
+	if !config.SkipComment {
+		gha.StartLogGroup("Processing comment...")
+		existingComment := checkExistingComments(event, config, ctx)
+		buildComment := ghc.ProcessFlags(flagsRef, flags, config)
+		postedComments = ghc.BuildFlagComment(buildComment, flagsRef, existingComment)
+		if postedComments != "" {
+			comment := github.IssueComment{
+				Body: &postedComments,
+			}
 
-		err = postGithubComment(ctx, flagsRef, config, existingComment, *event.PullRequest.Number, comment)
+			err = postGithubComment(ctx, flagsRef, config, existingComment, *event.PullRequest.Number, comment)
+		}
+		gha.EndLogGroup()
 	}
-	gha.EndLogGroup()
 
 	// Add flag links
 	if config.CreateFlagLinks && postedComments != "" {
